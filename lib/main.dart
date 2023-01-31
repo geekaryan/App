84f4acb12729e7987d9205e04cccab199adbf6ef
geekaryan <aryan.rana2021@vitbhopal.ac.in>
2023-02-01 00:57:49 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+//variables inside classes are called properties..
+//functions inside classes are called methods..
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text("Hello Sudocodes is here..!"),
+    );
+  }
+}
